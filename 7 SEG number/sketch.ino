0223ca7308c80b080a7c6c635e26d2676111ab03
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2024-04-09 14:20:33 +0300

    org

---
+++ 7 SEG number/sketch.ino
@@ -0,0 +1,46 @@
+#define ROW 10
+#define COL 7
+
+const int mapNumbers[ROW][COL] = {
+  // A  B  C  D  E  F  G
+  {1, 1, 1, 1, 1, 1, 0},
+  {0, 1, 1, 0, 0, 0, 0},
+  {1, 1, 0, 1, 1, 0, 1},
+  {1, 1, 1, 1, 0, 0, 1},
+  {0, 1, 1, 0, 0, 1, 1},
+  {1, 0, 1, 1, 0, 1, 1},
+  {1, 0, 1, 1, 1, 1, 1},
+  {1, 1, 1, 0, 0, 0, 0},
+  {1, 1, 1, 1, 1, 1, 1},
+  {1, 1, 1, 1, 0, 1, 1},
+};
+
+const int A = 2;
+const int B = 3;
+const int C = 4;
+const int D = 5;
+const int E = 6;
+const int F = 7;
+const int G = 8;
+const int DP = 9;
+
+void setup() {
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
+  pinMode(F, OUTPUT);
+  pinMode(G, OUTPUT);
+  pinMode(DP, OUTPUT);
+}
+
+void loop() {
+  for (int i = 0; i < 10; i++) {
+	for (int j = 0; i < 7; i++) {
+		digitalWrite(A, mapNumbers[i][j] );
+    delay(1000);
+  }
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Embedded_Programing_Arduino_C_2024B
-open-source embedded system development platform course (Ariel University) is designed to teach software engineering students with a Java background the art of  Embedded Programming with Arduino Uno that has gained huge amount of popularity in past few years, specially among hobbyists. (implementation in C)
+# Arduino_2024B
+
+
+open-source embedded system development platform course (Ariel University) is designed to teach software engineering students with a Java background the art of Embedded Programming with Arduino Uno that has gained huge amount of popularity in past few years, specially among hobbyists. (implementation in C)
